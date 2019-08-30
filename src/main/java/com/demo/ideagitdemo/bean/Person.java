a175ecfa173c2ba472b7a7961e91bb7893ca5d24
fengGitHub8 <5148462+fengGitee129@user.noreply.gitee.com>
2019-08-30 09:46:52 +0800

    person第一次提交

---
+++ src/main/java/com/demo/ideagitdemo/bean/Person.java
@@ -0,0 +1,10 @@
+package com.demo.ideagitdemo.bean;
+
+/**
+ * @program: idea-git-demo
+ * @description: person实体类
+ * @author: fengyang
+ * @create: 2019-08-30 09:45
+ **/
+public class Person {
+}
